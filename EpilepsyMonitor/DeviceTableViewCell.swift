243d40918f72235fe190bc7aaa197351d227ee04
chad <chapnickc@gmail.com>
2016-08-10 13:07:51 -0500

    scans for devices and displays name in table view

---
+++ EpilepsyMonitor/DeviceTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  DeviceTableViewCell.swift
+//  EpilepsyMonitor
+//
+//  Created by chapnickc on 8/10/16.
+//  Copyright © 2016 Chad. All rights reserved.
+//
+
+import UIKit
+
+class DeviceTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    
+    var displayPeripheral: DisplayPeripheral? {
+		didSet {
+			if let deviceName = displayPeripheral!.peripheral?.name{
+				deviceNameLabel.text = deviceName.isEmpty ? "No Device Name" : deviceName
+			}
+            else{
+				deviceNameLabel.text = "No Device Name"
+			}
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- EpilepsyMonitor/ViewController.swift
+++ EpilepsyMonitor/ViewController.swift
@@ -7,18 +7,82 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
 
+struct DisplayPeripheral{
+	var peripheral: CBPeripheral?
+	var lastRSSI: NSNumber?
+	var isConnectable: Bool?
+}
+
+
+
+
+class ViewController: UIViewController, UITableViewDataSource, CBCentralManagerDelegate {
+
+    var centralManager: CBCentralManager?
+    var peripherals: [DisplayPeripheral] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+		centralManager = CBCentralManager(delegate: self, queue: dispatch_get_main_queue())
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: CBCentralManagerDelegate
+   	func startScanning(){
+        print("Starting Scan")
+		peripherals = []
+		self.centralManager?.scanForPeripheralsWithServices(nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+	}
+	 
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        if (central.state == CBCentralManagerState.PoweredOn) {
+            startScanning()
+        }
+        else {
+            print("Please Enable Bluetooth")
+        }
     }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+    	for (index, foundPeripheral) in peripherals.enumerate(){
+			if foundPeripheral.peripheral?.identifier == peripheral.identifier{
+				peripherals[index].lastRSSI = RSSI
+				return
+			}
+		}
+        
+		let isConnectable = advertisementData["kCBAdvDataIsConnectable"] as! Bool
+		let displayPeripheral = DisplayPeripheral(peripheral: peripheral, lastRSSI: RSSI, isConnectable: isConnectable)
+		peripherals.append(displayPeripheral)
+		tableView.reloadData()
+        print("Added \(peripheral.name)")
+    }
+    
+    // MARK: UITableViewDataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peripherals.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "DeviceTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! DeviceTableViewCell
+        
+        cell.displayPeripheral = peripherals[indexPath.row]
+        return cell
+    }
+   
 
 
 }
